18c043b1b529b4e5f5615cbee54c25c67c6d355c
LUCA COSTA s290230 <s290230@studenti.polito.it>
2022-05-28 19:05:10 +0200

    TicketRouter and the ReactiveCrudRepository works fine

--- src/main/kotlin/it/group24/lab5/webapp2/ticketcatalogue/repository/TicketRepository.kt
+++ src/main/kotlin/it/group24/lab5/webapp2/ticketcatalogue/repository/TicketRepository.kt
@@ -1,9 +1,16 @@
 package it.group24.lab5.webapp2.ticketcatalogue.repository
 
 import it.group24.lab5.webapp2.ticketcatalogue.domain.Ticket
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
-import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.data.r2dbc.repository.Query
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.r2dbc.core.DatabaseClient
+import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
 
-interface TicketRepository: CoroutineCrudRepository<Ticket, Long> {
+
+interface TicketRepository: ReactiveCrudRepository<Ticket, Long> {
+
 
 }
